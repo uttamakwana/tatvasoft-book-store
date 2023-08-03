62430dd99610fe571fa5311c64bf1be3f4ad6414
Uttam Makwana <uttamakwana4503@gmail.com>
2023-08-03 11:17:12 +0530

    Changed theme colors in Light mode in Theme Provider

--- src/theme/ThemeProvider.jsx
+++ src/theme/ThemeProvider.jsx
@@ -7,15 +7,15 @@ const ThemeProvider = () => {
   const { theme, setTheme } = useContext(Context);
   useEffect(() => {
     if (theme === "light") {
-      document.documentElement.style.setProperty("--color-primary", "#4682A9");
+      document.documentElement.style.setProperty("--color-primary", "#FFF6E0");
       document.documentElement.style.setProperty(
         "--color-secondary",
-        "#749BC2"
+        "#D8D9DA"
       );
-      document.documentElement.style.setProperty("--color-tertiary", "#91C8E4");
+      document.documentElement.style.setProperty("--color-tertiary", "#61677A");
       document.documentElement.style.setProperty(
         "--color-quaternary",
-        "#F6F4EB"
+        "#272829"
       );
     } else {
       document.documentElement.style.setProperty("--color-primary", "#191919");
